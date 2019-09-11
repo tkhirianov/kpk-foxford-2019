d4381a30d899acc9f6601649f3afe34a1233fe51
Timofey Khirianov <tkhirianov@gmail.com>
2019-09-11 19:00:42 +0300

    Проработана функция, рисующая всё и брошены заготовки функций рисования объектов.

--- picture_2.py
+++ picture_2.py
@@ -1,9 +1,29 @@
 # coding: utf-8
 from graph import *
 
+def paint_background():
+    pass
+
+def paint_house():
+    pass
+
+def paint_tree():
+    pass
+
+def paint_cloud():
+    pass
+
+def paint_sun():
+    pass
+
+
 def paint_all():
     """ Должна нарисовать всё. """
-    print("Должна нарисовать всё. Но нет...")
+    paint_background()
+    paint_house()
+    paint_tree()
+    paint_cloud()
+    paint_sun()
 
 windowSize(450, 300)
 paint_all()
