9261ab9bcb44a5c3ad1df61ace33619288872b30
Timofey Khirianov <tkhirianov@gmail.com>
2019-09-11 18:51:44 +0300

    Сделал большую функцию для рисования всего.

--- picture_2.py
+++ picture_2.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from graph import *
 
-
-
+def paint_all():
+    """ Должна нарисовать всё. """
+    print("Должна нарисовать всё. Но нет...")
 
 windowSize(450, 300)
-
+paint_all()
 run()
